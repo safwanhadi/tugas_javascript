92e724d832b2b3df86b9dbed1a8c37e28a248f41
safwanhadi <safwanhadi.muhammad@gmail.com>
2019-07-25 15:43:42 +0800

    menambahkan file tugas2

---
+++ tugas2.js
@@ -0,0 +1,16 @@
+var number = 150;
+var nilai_null = null;
+var nilai_undefined;
+if (number < 170)
+    nilai = true;
+else
+    nilai = false;
+
+var number_array = [2, 4, 6, 8, 10];
+
+console.log("variabel Number:", number);
+console.log("variabel null:", nilai_null);
+console.log("variabel undefined:", nilai_undefined);
+console.log("variabel boolean:", nilai);
+console.log("variabel array:", number_array[4]);
+console.log("variabel undefined dari array:", number_array[5]);
