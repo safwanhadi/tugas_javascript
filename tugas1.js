fd61d138e448829399199429a7b774181c3260e3
safwanhadi <safwanhadi.muhammad@gmail.com>
2019-07-25 14:57:54 +0800

    menambahkan file tugas1

---
+++ tugas1.js
@@ -0,0 +1,7 @@
+var nama_depan = "Muh. Safwan";
+let nama_belakang = "hadi";
+const usia = 35;
+
+console.log("nama:", nama_depan, nama_belakang)
+//console.log(nama_belakang)
+console.log("usia:", usia)
